1eadfb458b6bde5abc2c210edfeef27b3148cf98
Anders Bech Mellson <mellson@icloud.com>
2021-11-03 07:14:24 +0100

    updated

--- src/index.bs.js
+++ src/index.bs.js
@@ -4,13 +4,13 @@ import * as React from "react";
 import * as ReactDom from "react-dom";
 import * as Belt_Option from "rescript/lib/es6/belt_Option.js";
 import * as Caml_option from "rescript/lib/es6/caml_option.js";
-import * as App$RescriptIntro from "./App.bs.js";
+import * as App$RescriptTypeInferenceExample from "./App.bs.js";
 
 import './index.css'
 ;
 
 ReactDom.render(React.createElement(React.StrictMode, {
-          children: React.createElement(App$RescriptIntro.make, {})
+          children: React.createElement(App$RescriptTypeInferenceExample.make, {})
         }), Belt_Option.getExn(Caml_option.nullable_to_opt(document.querySelector("#root"))));
 
 export {
